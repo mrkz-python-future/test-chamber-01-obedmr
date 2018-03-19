9a8446050687767666f817e657586d9e4b48f90c
Marcos Simental <mrkzmrkz@gmail.com>
2018-03-19 16:12:47 -0600

    add python code example

---
+++ hello-world.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+#encoding=utf-8
+
+# print is now a function, meaning that we must
+# use parenthesis "()" every time we print something to
+# the screen
+#
+# https://docs.python.org/3.5/whatsnew/3.0.html#print-is-a-function
+print("Hello world from python3")
